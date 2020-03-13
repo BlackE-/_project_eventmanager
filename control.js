618427dc5687b5d023826410d50f02e4ad526f69
BlackE <elizabeth.ramescamilla@gmail.com>
2020-03-13 01:37:17 -0600

    feat(sidebar): add siderbar, control and more style in dark mode

--- control.js
+++ control.js
@@ -1,4 +1,4 @@
-const modal = document.getElementById('myModal');
+  const modal = document.getElementById('myModal');
   		const close = document.getElementsByClassName("close")[0];
   		document.addEventListener('click',function(e){
   			// console.log(e.target);
@@ -8,11 +8,23 @@ const modal = document.getElementById('myModal');
      			const eventDateOrigin = e.target.title;
      			let eventDate = new Date(eventDateOrigin);
      			// let date_ = `${eventDate.getFullYear()}-${eventDate.getMonth()+1}-${eventDate.getDate()}`;
-     			let date_ = formatDate(eventDate);
+     			let date_ = calendar_._formateDate(eventDate);
      			// console.log(date_);
   				modal.style.display = 'block';
   				document.querySelector('input[name="modal_date"]').value = date_;
      		}
  		});
 
-  		close.onclick = function() {modal.style.display = "none";}
+  		close.onclick = function() {modal.style.display = 'none';}
+
+      document.querySelector('input[name="siderbar_button"]').addEventListener('change',function(){
+        // console.log(this.checked);
+        if(this.checked) {
+          document.getElementById('sidebar').style.width = '50px';
+          document.getElementById('main').style.marginLeft = '50px';  
+        }
+        else{
+          document.getElementById('sidebar').style.width = '150px';
+          document.getElementById('main').style.marginLeft = '150px';
+        }
+      });
